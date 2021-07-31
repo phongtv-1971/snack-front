a7f20db01024aeccec14f8535cf4e1b3ca8d84a4
phongtv-1971 <truong.van.phong@sun-asterisk.com>
2021-07-31 23:26:52 +0700

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    }
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react-hooks",
+    "jest",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  ignorePatterns: ["**/generated/**"],
+  rules: {
+    "eqeqeq": ["error"],
+    "no-case-declarations": ["off"],
+    "no-console": ["error", { allow: ["error"] }],
+    "no-prototype-builtins": ["off"],
+    "no-unused-vars": ["off"],
+    "no-var": ["error"],
+
+    /* ts */
+    "@typescript-eslint/camelcase": ["off"],
+    "@typescript-eslint/explicit-function-return-type": ["off"],
+    "@typescript-eslint/interface-name-prefix": ["off"],
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/no-inferrable-types": ["off"],
+    "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "argsIgnorePattern": "^_", "ignoreRestSiblings": true }],
+    "@typescript-eslint/no-use-before-define": ["off"],
+    "@typescript-eslint/explicit-module-boundary-types": ["off"],
+
+    /* React */
+    "react/display-name": ["off"],
+    "react/prop-types": ["off"],
+
+    /* Jest */
+    "jest/expect-expect": ["off"],
+    "jest/no-commented-out-tests": ["off"],
+  },
+}
